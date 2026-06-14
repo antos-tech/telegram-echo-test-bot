f0f6d3a59d1d0fdd4383ab3c67bed66dc3f08e0b
Anto's Tech 📱 <antoniomorg2011@gmail.com>
2026-06-14 10:28:38 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import Application, MessageHandler, ContextTypes, filters
 
-TOKEN = os.environ.get("8632711070:AAE4Er4O73XFh8ct1Hjy2KcPNrSyc4suJMU")
+TOKEN = os.environ.get("BOT_TOKEN")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(update.message.text)
